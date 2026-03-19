fcee35551543986b1d0efd80f47854e3ccf03068
Freddie Ventura <fventura59@gmail.com>
2026-03-19 16:50:26 -0500

    Added player
    Added player class
    
    Signed-off-by: Freddie Ventura <fventura59@gmail.com>

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state  # noqa: F401
+from player import Player
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     time = pygame.time.Clock()
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
 
@@ -19,9 +22,9 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = (time.tick(60)) / 1000
-        print(dt)
 
 
 if __name__ == "__main__":
