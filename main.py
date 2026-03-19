ff2256d431f56ef30d47116d1a2378b0ba57ad68
Freddie Ventura <fventura59@gmail.com>
2026-03-19 15:27:43 -0500

    adding player class

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    time = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
+        dt = (time.tick(60)) / 1000
+        print(dt)
 
 
 if __name__ == "__main__":
